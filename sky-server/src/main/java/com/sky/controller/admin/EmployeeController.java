a053c7bd2332d73ccb48bf6ef4217030805a20b2
wpt <wei_pengtao@qq.com>
2025-06-01 22:00:59 +0800

    【员工管理】删除自定义日志

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -35,8 +35,6 @@ public class EmployeeController {
 
     @PostMapping("/login")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
-        log.info("员工登录：{}", employeeLoginDTO);
-
         Employee employee = employeeService.login(employeeLoginDTO);
 
         //登录成功后，生成jwt令牌
@@ -59,7 +57,6 @@ public class EmployeeController {
 
     @PostMapping
     public Result<String> save(@RequestBody EmployeeDTO employeeDTO) {
-        log.info("新增员工：{}", employeeDTO);
         employeeService.save(employeeDTO);
         return Result.success();
     }
@@ -71,14 +68,12 @@ public class EmployeeController {
 
     @GetMapping("/page")
     public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
-        log.info("员工分页查询: {}", employeePageQueryDTO);
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
 
     @PostMapping("/status/{status}")
     public Result<String> startOrStop(Long id, @PathVariable Integer status) {
-        log.info("启用禁用员工 {} 账号: {}", id, status);
         employeeService.startOrStop(id, status);
         return Result.success();
     }
