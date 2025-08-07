d06be3e0c55774ab4d2b30eacc54ea1004b7e8f1
wpt <wei_pengtao@qq.com>
2025-08-07 20:48:17 +0800

    完善登录功能

--- employee-service/src/main/java/com/sky/employee/handler/AutoFillHandler.java
+++ employee-service/src/main/java/com/sky/employee/handler/AutoFillHandler.java
@@ -1,6 +1,7 @@
 package com.sky.employee.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.sky.employee.context.BaseContext;
 import org.apache.ibatis.reflection.MetaObject;
 
 import java.time.LocalDateTime;
@@ -10,15 +11,15 @@ public class AutoFillHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "createUser", Long.class, 1L);
+        this.strictInsertFill(metaObject, "createUser", Long.class, BaseContext.getCurrentUser());
         this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "updateUser", Long.class, 1L);
+        this.strictInsertFill(metaObject, "updateUser", Long.class, BaseContext.getCurrentUser());
     }
 
     // 更新时自动填充
     @Override
     public void updateFill(MetaObject metaObject) {
         this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
-        this.strictUpdateFill(metaObject, "updateUser", Long.class, 1L);
+        this.strictUpdateFill(metaObject, "updateUser", Long.class, BaseContext.getCurrentUser());
     }
 }
